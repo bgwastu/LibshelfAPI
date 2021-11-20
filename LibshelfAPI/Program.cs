b0843a48c1e5820866f8494d9da9c5ef3a90adc4
Atticdev <67826350+atticdev@users.noreply.github.com>
2021-11-20 22:14:07 +0700

    Set connection string for PostgreSQL
    Set connection string for PostgreSQL to Heroku when production

--- LibshelfAPI/Program.cs
+++ LibshelfAPI/Program.cs
@@ -9,8 +9,30 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<LibshelfContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Heroku
+var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development";
+var connectionString = isDevelopment
+    ? builder.Configuration.GetConnectionString("DefaultConnection")
+    : GetHerokuConnectionString();
+
+static string? GetHerokuConnectionString()
+{
+    var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    if (connectionUrl != null)
+    {
+        var databaseUri = new Uri(connectionUrl);
+
+        var db = databaseUri.LocalPath.TrimStart('/');
+        var userInfo = databaseUri.UserInfo.Split(':', StringSplitOptions.RemoveEmptyEntries);
+
+        return $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};Port={databaseUri.Port};Database={db};Pooling=true;SSL Mode=Require;Trust Server Certificate=True;";
+    }
+
+    return null;
+}
+
+builder.Services.AddDbContext<LibshelfContext>(options => options.UseNpgsql(connectionString));
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
