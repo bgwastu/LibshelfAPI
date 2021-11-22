c7f5bf9ab792187df89f3c5635d22e08939fda9d
Atticdev <67826350+atticdev@users.noreply.github.com>
2021-11-22 11:36:33 +0700

    Edit connection string

--- LibshelfAPI/Program.cs
+++ LibshelfAPI/Program.cs
@@ -9,28 +9,7 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Heroku
-var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development";
-var connectionString = isDevelopment
-    ? builder.Configuration.GetConnectionString("DefaultConnection")
-    : GetHerokuConnectionString();
-
-static string? GetHerokuConnectionString()
-{
-    var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
-    if (connectionUrl != null)
-    {
-        var databaseUri = new Uri(connectionUrl);
-
-        var db = databaseUri.LocalPath.TrimStart('/');
-        var userInfo = databaseUri.UserInfo.Split(':', StringSplitOptions.RemoveEmptyEntries);
-
-        return $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};Port={databaseUri.Port};Database={db};Pooling=true;SSL Mode=Require;Trust Server Certificate=True;";
-    }
-
-    return null;
-}
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<LibshelfContext>(options => options.UseNpgsql(connectionString));
 
